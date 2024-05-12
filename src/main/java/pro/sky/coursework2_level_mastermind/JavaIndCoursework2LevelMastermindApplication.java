81ee3399ebbd1f1092787f44bba1c6351ef722eb
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-05-12 21:43:41 +0300

    Курсовая работа 2. Уровень Mastermind - начальный коммит

---
+++ src/main/java/pro/sky/coursework2_level_mastermind/JavaIndCoursework2LevelMastermindApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.coursework2_level_mastermind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIndCoursework2LevelMastermindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIndCoursework2LevelMastermindApplication.class, args);
+	}
+
+}
